f557a12c39f368cb60ac7e03f6a022ea625a6c4b
Camilo Morón <atontaurus@gmail.com>
2022-06-02 17:57:51 -0400

    Union de los dos problemas

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,20 @@ public class Main {
 
         int valor = suma(a,b,c);
         System.out.println(valor);
-    }
 
+        Coche miCarro = new Coche();
+        miCarro.aumentarPuertas();
+        System.out.println("El número de puertas es :  " + miCarro.puertas);
+    }
     public static int suma(int a, int b, int c)   {
         int i = a + b + c;
         return i;
+    }
+    class Coche {
+        public int puertas = 4;
+        public void aumentarPuertas() {
+            this.puertas++;
+        }
     }
 
 }
